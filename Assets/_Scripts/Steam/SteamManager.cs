f59b08d266573462cd4b75ec44f144199ebf5f68
Boxfriend <cawills394@gmail.com>
2023-07-22 00:17:55 -0400

    created SteamManager class to test steam stuff

---
+++ Assets/_Scripts/Steam/SteamManager.cs
@@ -0,0 +1,28 @@
+﻿using Steamworks;
+using UnityEngine;
+using Serilog;
+
+namespace Boxfriend.Steam
+{
+    public class SteamManager : MonoBehaviour
+    {
+        public void OpenInviteOverlay()
+        {
+            if(SteamClient.IsValid)
+            {
+                try
+                {
+                    SteamFriends.OpenGameInviteOverlay(SteamClient.SteamId);
+                }
+                catch (System.Exception e)
+                {
+                    Log.Fatal($"STEAMWORKS: {e.Message}");
+                }
+            }
+            else
+            {
+                Log.Information("STEAMWORKS: Steam not initialized.");
+            }
+        }
+    }
+}
