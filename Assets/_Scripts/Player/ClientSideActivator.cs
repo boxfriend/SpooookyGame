3a8449e21bc9fc9ecfa174de83811dfe87ee76d9
Boxfriend <cawills394@gmail.com>
2023-07-25 21:35:37 -0400

    created client side activator component

---
+++ Assets/_Scripts/Player/ClientSideActivator.cs
@@ -0,0 +1,32 @@
+﻿using Cinemachine;
+using NaughtyAttributes;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Boxfriend.Player
+{
+    public class ClientSideActivator : NetworkBehaviour
+    {
+        [SerializeField] private CinemachineVirtualCamera _vcam;
+
+        public override void OnNetworkSpawn()
+        {
+            if(IsLocalPlayer)
+            {
+                ActivateClients();
+            }
+        }
+
+        [ContextMenu("ActivateClient")]
+        private void ActivateClients()
+        {
+            var clientSides = GetComponentsInChildren<IClientSideComponent>(true);
+            foreach (var client in clientSides)
+            {
+                client.ActivateClient();
+            }
+
+            _vcam.enabled = true;
+        }
+    }
+}
