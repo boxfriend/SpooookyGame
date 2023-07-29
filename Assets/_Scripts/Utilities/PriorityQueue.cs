ece8d6f81bc63d5cff80a3c1cc3de63e80c698fa
Boxfriend <cawills394@gmail.com>
2023-07-29 04:16:14 -0400

    node for linked list implementation

---
+++ Assets/_Scripts/Utilities/PriorityQueue.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+namespace Boxfriend.Utilities
+{
+    public class PriorityQueue<T>
+    {
+        private class QueueNode
+        {
+            public T Item { get; }
+            public float BasePriority { get; }
+            public float Priority { get; set; }
+
+            public QueueNode Previous { get; set; }
+            public QueueNode Next { get; set; }
+
+            public QueueNode(T item, float basePriority)
+            {
+                Item = item;
+                Priority = basePriority;
+                BasePriority = basePriority;
+            }
+
+            public QueueNode (T item, float basePriority, QueueNode previous, QueueNode next) : this(item, basePriority)
+            {
+                Previous = previous;
+                Next = next;
+            }
+        }
+    }
+}
