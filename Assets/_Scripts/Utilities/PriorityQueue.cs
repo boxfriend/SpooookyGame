28ca0804b1714463a01600e9762fba8a2c7abaed
Boxfriend <cawills394@gmail.com>
2023-07-29 04:17:43 -0400

    in place sorting when adding nodes initially

--- Assets/_Scripts/Utilities/PriorityQueue.cs
+++ Assets/_Scripts/Utilities/PriorityQueue.cs
@@ -9,6 +9,66 @@ namespace Boxfriend.Utilities
         private QueueNode _head = null;
 
         public int Count => _contained.Count;
+        public bool TryEnqueue(T item, float basePriority)
+        {
+            if (_contained.ContainsKey(item))
+                return false;
+
+            var node = new QueueNode(item, basePriority, null, _head);
+            if(_head  != null)
+                _head.Previous = node;
+
+            _head = node;
+            _contained.Add(item, node);
+
+            UpdateNodePosition(node);
+            return true;
+        }
+        private void UpdateNodePosition(QueueNode node)
+        {
+            while (TryMoveLower(node));
+            while (TryMoveHigher(node));
+        }
+        private bool TryMoveLower (QueueNode node)
+        {
+            var next = node.Next;
+            if (next == null || next.Priority <= node.Priority)
+                return false;
+
+            if(node.Previous != null)
+                node.Previous.Next = next;
+
+            if (node == _head)
+                _head = next;
+
+            node.Next = next.Next;
+            next.Next = node;
+            next.Previous = node.Previous;
+            node.Previous = next;
+
+            return true;
+        }
+
+        private bool TryMoveHigher (QueueNode node)
+        {
+            var prev = node.Previous;
+            if(prev == null || prev.Priority >= node.Priority)
+                return false;
+
+            if (prev == _head)
+                _head = node;
+
+            if(node.Next != null)
+                node.Next.Previous = prev;
+
+            node.Previous = prev.Previous;
+            prev.Previous = node;
+            prev.Next = node.Next;
+            node.Next = prev;
+
+            return true;
+        }
+
         private class QueueNode
         {
             public T Item { get; }
