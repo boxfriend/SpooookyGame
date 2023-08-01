6a49910ef5f70400a77d9652d80d14b4719bdbe9
Boxfriend <cawills394@gmail.com>
2023-08-01 03:42:17 -0400

    client authority components

---
+++ Assets/_Scripts/Networking/ClientAuthority/ClientNetworkAnimator.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Netcode.Components;
+
+namespace Boxfriend.Networking.ClientAuthority
+{
+    public class ClientNetworkAnimator : NetworkAnimator
+    {
+        protected override bool OnIsServerAuthoritative () => false;
+
+        public override void OnGainedOwnership () => base.OnGainedOwnership();
+    }
+}
---
+++ Assets/_Scripts/Networking/ClientAuthority/ClientNetworkTransform.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Netcode.Components;
+
+namespace Boxfriend.Networking.ClientAuthority
+{
+    public class ClientNetworkTransform : NetworkTransform
+    {
+        protected override bool OnIsServerAuthoritative () => false;
+    }
+}
